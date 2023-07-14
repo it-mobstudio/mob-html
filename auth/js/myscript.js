b2dd2648b27cf4001c9bde6ced40c42058f1ba81
Naresh <naresh@madoverbuildings.com>
2023-07-14 09:26:34 +0530

    work on adding users and role create pages

---
+++ auth/js/myscript.js
@@ -0,0 +1,13 @@
+function addLinkClass() {
+    var link = document.getElementById("manage_role");
+    link.classList.add("active");
+  }
+
+  function countEntries() {
+    var table = document.getElementById("dataTableid");
+    var rowCount = table.rows.length - 1; // Exclude the header row
+    var entryCountElement = document.getElementById("dataTableid_info");
+    entryCountElement.textContent = "Showing 1 to " + rowCount + " of " + rowCount + " entries";
+  }
+  
+  countEntries();
